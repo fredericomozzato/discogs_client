edb4a0a55049c5f7393c8d7ebd249435daf808ed
Frederico <frederico@Fredericos-MacBook-Air.local>
2025-12-25 11:03:45 -0300

    Reorder models and add meaningful comments

--- discogs/models.go
+++ discogs/models.go
@@ -2,99 +2,14 @@ package discogs
 
 import "time"
 
-// TODO: the fields here are only relevant to the Serendipity application. We should
-// ensure that this client captures the whole data from the payload.
-
-type Artist struct {
-	Name        string `json:"name"`
-	Anv         string `json:"anv"`
-	Join        string `json:"join"`
-	Role        string `json:"role"`
-	Tracks      string `json:"tracks"`
-	ID          int64  `json:"id"`
-	ResourceURL string `json:"resource_url"`
-	ThumbURL    string `json:"thumbnail_url"`
-}
-
-type Community struct {
-	Have         int64  `json:"have"`
-	Want         int64  `json:"want"`
-	Rating       Rating `json:"rating"`
-	Submitter    User   `json:"submitter"`
-	Contributors []User `json:"contributors"`
-	DataQuality  string `json:"data_quality"`
-	Status       string `json:"status"`
-}
-
-type Company struct {
-	Name           string `json:"name"`
-	Catno          string `json:"catno"`
-	EntityType     string `json:"entity_type"`
-	EntityTypeName string `json:"entity_type_name"`
-	ID             int64  `json:"id"`
-	ResourceURL    string `json:"resource_url"`
-	ThumbURL       string `json:"thumbnail_url"`
-}
-
-type Format struct {
-	Name         string   `json:"name"`
-	Quantity     string   `json:"qty"`
-	Descriptions []string `json:"descriptions"`
-	Text         string   `json:"text"`
-}
-
-type Identifier struct {
-	Type        string `json:"type"`
-	Value       string `json:"value"`
-	Description string `json:"description"`
-}
-
-type Image struct {
-	Type        string `json:"type"`
-	URI         string `json:"uri"`
-	ResourceURL string `json:"resource_url"`
-	URI150      string `json:"uri150"`
-	Width       int    `json:"width"`
-	Height      int    `json:"height"`
-}
-
-type Label struct {
-	Name           string `json:"name"`
-	Catno          string `json:"catno"`
-	EntityType     string `json:"entity_type"`
-	EntityTypeName string `json:"entity_type_name"`
-	ID             int64  `json:"id"`
-	ResourceURL    string `json:"resource_url"`
-	ThumbURL       string `json:"thumbnail_url"`
-}
-
-type Track struct {
-	Position     string   `json:"position"`
-	Type         string   `json:"type_"`
-	Title        string   `json:"title"`
-	ExtraArtists []Artist `json:"extraartists"`
-	Duration     string   `json:"duration"`
-}
-
-type Video struct {
-	URI         string `json:"uri"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Duration    int    `json:"duration"`
-	Embed       bool   `json:"embed"`
-}
-
-type Rating struct {
-	Count   int64   `json:"count"`
-	Average float32 `json:"average"`
-}
-
+// Release represents a Discogs release (album, single, etc.)
 type Release struct {
 	ID                   int64        `json:"id"`
 	Status               string       `json:"status"`
 	Year                 int          `json:"year"`
 	ResourceURL          string       `json:"resource_url"`
 	URI                  string       `json:"uri"`
+	Artists              []Artist     `json:"artists"`
 	ArtistsSort          string       `json:"artists_sort"`
 	Labels               []Label      `json:"labels"`
 	Companies            []Company    `json:"companies"`
@@ -123,16 +38,106 @@ type Release struct {
 	Thumb                string       `json:"thumb"`
 	EstimatedWeight      int          `json:"estimated_weight"`
 	BlockedFromSale      bool         `json:"blocked_from_sale"`
-	Artists              []Artist     `json:"artists"`
 	IsOffensive          bool         `json:"is_offensive"`
-
-	// Still couldn't find a release with a populated "Series" field
-	// so this will convert anything until we cna be sure of what this
-	// this field contains to properly parse it.
-	Series []map[string]any `json:"series"`
+	Series               []map[string]any `json:"series"`
 }
 
+// Artist represents an artist, contributor, or performer
+type Artist struct {
+	Name        string `json:"name"`
+	Anv         string `json:"anv"`
+	Join        string `json:"join"`
+	Role        string `json:"role"`
+	Tracks      string `json:"tracks"`
+	ID          int64  `json:"id"`
+	ResourceURL string `json:"resource_url"`
+	ThumbURL    string `json:"thumbnail_url"`
+}
+
+// Track represents a track on a release
+type Track struct {
+	Position     string   `json:"position"`
+	Type         string   `json:"type_"`
+	Title        string   `json:"title"`
+	ExtraArtists []Artist `json:"extraartists"`
+	Duration     string   `json:"duration"`
+}
+
+// Community represents community data for a release
+type Community struct {
+	Have         int64  `json:"have"`
+	Want         int64  `json:"want"`
+	Rating       Rating `json:"rating"`
+	Submitter    User   `json:"submitter"`
+	Contributors []User `json:"contributors"`
+	DataQuality  string `json:"data_quality"`
+	Status       string `json:"status"`
+}
+
+// Rating represents the community rating for a release
+type Rating struct {
+	Count   int64   `json:"count"`
+	Average float32 `json:"average"`
+}
+
+// User represents a Discogs user
 type User struct {
 	Username    string `json:"username"`
 	ResourceURL string `json:"resource_url"`
+}
+
+// Company represents a company involved in a release
+type Company struct {
+	Name           string `json:"name"`
+	Catno          string `json:"catno"`
+	EntityType     string `json:"entity_type"`
+	EntityTypeName string `json:"entity_type_name"`
+	ID             int64  `json:"id"`
+	ResourceURL    string `json:"resource_url"`
+	ThumbURL       string `json:"thumbnail_url"`
+}
+
+// Label represents a record label
+type Label struct {
+	Name           string `json:"name"`
+	Catno          string `json:"catno"`
+	EntityType     string `json:"entity_type"`
+	EntityTypeName string `json:"entity_type_name"`
+	ID             int64  `json:"id"`
+	ResourceURL    string `json:"resource_url"`
+	ThumbURL       string `json:"thumbnail_url"`
+}
+
+// Format represents the physical format of a release
+type Format struct {
+	Name         string   `json:"name"`
+	Quantity     string   `json:"qty"`
+	Descriptions []string `json:"descriptions"`
+	Text         string   `json:"text"`
+}
+
+// Video represents a video associated with a release
+type Video struct {
+	URI         string `json:"uri"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Duration    int    `json:"duration"`
+	Embed       bool   `json:"embed"`
+}
+
+// Image represents an image of a release
+type Image struct {
+	Type        string `json:"type"`
+	URI         string `json:"uri"`
+	ResourceURL string `json:"resource_url"`
+	URI150      string `json:"uri150"`
+	Width       int    `json:"width"`
+	Height      int    `json:"height"`
+}
+
+// Identifier represents a barcode, matrix number, or other identifier
+type Identifier struct {
+	Type        string `json:"type"`
+	Value       string `json:"value"`
+	Description string `json:"description"`
 }
