f2b698adde84e654e988cb85edcbbf108f819982
Frederico Mozzato <frederico.smozzato@gmail.com>
2026-01-08 20:19:09 -0300

    Reorganize structure with facade pattern

---
+++ models.go
@@ -0,0 +1,16 @@
+package discogsclient
+
+import "github.com/fredericomozzato/discogs_client/internal/discogs"
+
+type Release = discogs.Release
+type Artist = discogs.Artist
+type Track = discogs.Track
+type Community = discogs.Community
+type Rating = discogs.Rating
+type User = discogs.User
+type Company = discogs.Company
+type Label = discogs.Label
+type Format = discogs.Format
+type Video = discogs.Video
+type Image = discogs.Image
+type Identifier = discogs.Identifier
